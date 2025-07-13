8ba1318071fdd7f2a3281ec88ccdc102e79e5b33
Tnmae <ttyagi.2505@gmail.com>
2025-07-13 04:59:20 +0000

    meh

---
+++ build/CMakeFiles/OpenGLProject.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tanmay/Documents/cpp/GL_Con/lib/glad/glad.c" "CMakeFiles/OpenGLProject.dir/lib/glad/glad.c.o" "gcc" "CMakeFiles/OpenGLProject.dir/lib/glad/glad.c.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/Camera.cpp" "CMakeFiles/OpenGLProject.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/Camera.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/EBO.cpp" "CMakeFiles/OpenGLProject.dir/src/EBO.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/EBO.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/Mesh.cpp" "CMakeFiles/OpenGLProject.dir/src/Mesh.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/Mesh.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/Model.cpp" "CMakeFiles/OpenGLProject.dir/src/Model.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/Model.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/Sphere.cpp" "CMakeFiles/OpenGLProject.dir/src/Sphere.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/Sphere.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/Texture.cpp" "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/VAO.cpp" "CMakeFiles/OpenGLProject.dir/src/VAO.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/VAO.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/VBO.cpp" "CMakeFiles/OpenGLProject.dir/src/VBO.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/VBO.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/main.cpp" "CMakeFiles/OpenGLProject.dir/src/main.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/main.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/shader.cpp" "CMakeFiles/OpenGLProject.dir/src/shader.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/shader.cpp.o.d"
+  "/home/tanmay/Documents/cpp/GL_Con/src/stb_image_imp.cpp" "CMakeFiles/OpenGLProject.dir/src/stb_image_imp.cpp.o" "gcc" "CMakeFiles/OpenGLProject.dir/src/stb_image_imp.cpp.o.d"
+  "" "OpenGLProject" "gcc" "CMakeFiles/OpenGLProject.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLProject.dir/link.d"
+  "CMakeFiles/OpenGLProject.dir/lib/glad/glad.c.o"
+  "CMakeFiles/OpenGLProject.dir/lib/glad/glad.c.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Camera.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/EBO.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/EBO.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Mesh.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Mesh.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Model.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Model.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Sphere.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Sphere.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/VAO.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/VAO.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/VBO.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/VBO.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/main.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/shader.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/shader.cpp.o.d"
+  "CMakeFiles/OpenGLProject.dir/src/stb_image_imp.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/src/stb_image_imp.cpp.o.d"
+  "OpenGLProject"
+  "OpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
